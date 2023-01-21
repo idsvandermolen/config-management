86c3a3ccecf9b63289d360494ad5c1586811767f
Ids van der Molen <2285220+idsvandermolen@users.noreply.github.com>
2023-01-21 14:01:35 +0100

    Allow parameter overrides (#18)

--- lib/prometheus.py
+++ lib/prometheus.py
@@ -4,8 +4,8 @@ Generate Prometheus components
 from pathlib import Path
 import kubernetes.client as k
 from ruamel.yaml import YAML
-from . import util
 from datapath import DataPath
+from . import util
 
 
 def generate(dst: Path, components: Path, config: DataPath, stack_name: str):
@@ -17,14 +17,23 @@ def generate(dst: Path, components: Path, config: DataPath, stack_name: str):
     port = config.get(f"stacks.{stack_name}.prometheus.port", 9090)
     port_name = "api"
     # deployment
+    image = config.get(
+        f"stacks.{stack_name}.prometheus.image", "prom/prometheus:latest"
+    )
+    requests = config.get(
+        f"stacks.{stack_name}.prometheus.resources.requests",
+        {"cpu": "1", "memory": "1G"},
+    )
+    limits = config.get(
+        f"stacks.{stack_name}.prometheus.resources.limits",
+        {"cpu": "1", "memory": "1G"},
+    )
     deployment = util.mk_deployment(
         name,
         [
             k.V1Container(
                 name=name,
-                image=config.get(
-                    f"stacks.{stack_name}.prometheus.image", "prom/prometheus"
-                ),
+                image=image,
                 ports=[
                     k.V1ContainerPort(
                         name=port_name,
@@ -32,10 +41,8 @@ def generate(dst: Path, components: Path, config: DataPath, stack_name: str):
                     )
                 ],
                 resources=k.V1ResourceRequirements(
-                    requests=config[
-                        f"stacks.{stack_name}.prometheus.resources.requests"
-                    ],
-                    limits=config[f"stacks.{stack_name}.prometheus.resources.limits"],
+                    requests=requests,
+                    limits=limits,
                 ),
             )
         ],
@@ -50,8 +57,12 @@ def generate(dst: Path, components: Path, config: DataPath, stack_name: str):
         client.sanitize_for_serialization(
             util.mk_hpa(
                 name,
-                min_replicas=config[f"stacks.{stack_name}.prometheus.minReplicas"],
-                max_replicas=config[f"stacks.{stack_name}.prometheus.maxReplicas"],
+                min_replicas=config.get(
+                    f"stacks.{stack_name}.prometheus.minReplicas", 1
+                ),
+                max_replicas=config.get(
+                    f"stacks.{stack_name}.prometheus.maxReplicas", 1
+                ),
                 scale_target_ref=k.V1CrossVersionObjectReference(
                     api_version=deployment.api_version,
                     kind=deployment.kind,
@@ -76,6 +87,10 @@ def generate(dst: Path, components: Path, config: DataPath, stack_name: str):
         Path(output_dir / "service-account.yaml").open("w", encoding="utf-8"),
     )
     # ArgoCD app
-    app = DataPath(yaml.load(Path(components, "argocd", "prometheus.yaml").open(encoding="utf-8")))
+    app = DataPath(
+        yaml.load(Path(components, "argocd", "prometheus.yaml").open(encoding="utf-8"))
+    )
     app["spec.source.path"] = str(output_dir)
-    yaml.dump(app.data, Path(output_dir, "application.yaml").open("w", encoding="utf-8"))
+    yaml.dump(
+        app.data, Path(output_dir, "application.yaml").open("w", encoding="utf-8")
+    )
